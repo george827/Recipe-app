9d72658d1646f2ba96922c1347d3e50affac3203
george827 <kinyanjuigeorge827@gmail.com>
2023-04-19 11:04:16 +0300

    fix lonter error

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,7 +1,6 @@
 class Food < ApplicationRecord
   belongs_to :user
 
-
   validates :name, presence: true
   validates :measurement_unit, presence: true
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
